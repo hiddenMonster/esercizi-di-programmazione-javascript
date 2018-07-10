b87d603ac327fbc49ca5449ac78bf76dd0fa4cfe
Stefano <41064347+hiddenMonster@users.noreply.github.com>
2018-07-10 19:13:51 +0200

    Update conta-il-tempo.js
    Ciao, ho usato solo operatori aritmetici(modulo, divisione e sottrazione) e la concatenazione tra numeri e stringhe.
    Funziona con qualsiasi cifra in secondi inserita in input

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -1,8 +1,25 @@
 /*
   Conta il tempo
-  Scrivi un programma che prenda in input un numero di secondi e calcoli la quantità di ore, minuti e secondi corrispondenti, poi stampa il risultato. L'output avrà solo numeri interi.
+  Scrivi un programma che prenda in input un numero di secondi e calcoli la quantità di ore, minuti e secondi corrispondenti, 
+  poi stampa il risultato. L'output avrà solo numeri interi.
   Esempio
     Input: 12560
     Output: 3 ore, 29 minuti e 20 secondi.
   http://www.imparareaprogrammare.it
 */
+
+
+// ho usato solo operatori aritmetici(modulo, divisione e sottrazione) e la concatenazione tra numeri e stringhe
+// funziona con qualsiasi cifra in secondi inserita in input
+/****************
+"Conta il tempo"*
+10/07/2018      *
+By Ste          *
+*/
+var iInputTime = 12560; // original input time in seconds
+var iSeconds = iInputTime % 60;
+var iRealTime = iInputTime - iSeconds;
+var iMinutes = (iRealTime % 3600) / 60;
+iRealTime = iRealTime - (iRealTime % 3600);
+var iHours = (iRealTime / 3600);
+console.log(iHours + ' ore, ' + iMinutes + ' minuti, ' + iSeconds + ' secondi');
