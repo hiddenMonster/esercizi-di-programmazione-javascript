7aa2aa316253bed890aadfa569b9f285151a3d68
Stefano <41064347+hiddenMonster@users.noreply.github.com>
2018-07-11 12:22:46 +0200

    Update somma-e-media.js

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -3,3 +3,17 @@
   Scrivi un programma che prenda in input 5 numeri a tua scelta e restituisca in output la somma e la media.
   http://www.imparareaprogrammare.it
 */
+/****************
+"Somma e media" *
+11/07/2018      *
+By Ste          *
+*/
+var iNum1 = 10;
+var iNum2 = 25;
+var iNum3 = 98;
+var iNum4 = 5;
+var iNum5 = 41;
+
+var iSum = iNum1+iNum2+iNum3+iNum4+iNum5;
+var dAverage = iSum / 5;
+console.log('La somma dei numeri è: ' + iSum + ' ,la media è pari a: ' + dAverage);
