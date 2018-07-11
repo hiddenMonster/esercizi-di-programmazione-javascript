d37111b73e68662664e529158c594cd3fa563909
Stefano <41064347+hiddenMonster@users.noreply.github.com>
2018-07-11 09:40:50 +0200

    Update gatti-in-fila.js

--- esercizi-stringhe-numeri/gatti-in-fila.js
+++ esercizi-stringhe-numeri/gatti-in-fila.js
@@ -9,6 +9,18 @@
   Esempio
     Input: numero di gatti = 44, gatti per fila = 6
     Output: numero di file = 8, gatti mancanti = 4
-  Hint: Se vuoi usare l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero superiore solo da .5 in su) ma utilizza Math.ceil (arrotonda sempre all'intero superiore).
+  Hint: Se vuoi usare l'arrotondamento ti consiglio di non usare Math.round 
+  (arrotonda all'intero superiore solo da .5 in su) ma utilizza Math.ceil (arrotonda sempre all'intero superiore).
   http://www.imparareaprogrammare.it
 */
+/****************
+"Gatti in fila"*
+11/07/2018      *
+By Ste          *
+*/
+var iGatti = 44; // original integer input
+var iGattiPerFila = 6;   // original integer input
+var fGattiInFila = iGatti / iGattiPerFila;
+var iGattiInFila = Math.ceil(fGattiInFila);
+var iGattiMancanti = (iGattiPerFila * iGattiInFila) - iGatti;
+console.log('numero di file = ' + iGattiInFila + ', gatti mancanti = '+iGattiMancanti);
