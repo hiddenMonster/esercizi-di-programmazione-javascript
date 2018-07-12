4e404df2eba9dc65fb571a2d75e2cc760bfcc8e0
Stefano <41064347+hiddenMonster@users.noreply.github.com>
2018-07-12 17:32:36 +0200

    risolto

--- esercizi-condizioni/pari-o-dispari.js
+++ esercizi-condizioni/pari-o-dispari.js
@@ -2,4 +2,17 @@
   Pari o dispari?
   Scrivi un programma che prenda in input un numero intero e restituisca 0 se è pari o 1 se è dispari.
   http://www.imparareaprogrammare.it
+*//*****************
+"Pari o dispari"*
+12/07/2018       *
+By Ste           *
 */
+var iNum = 25; //original input
+if (iNum % 2)
+{
+  console.log('0');
+}
+else
+{
+  console.log('1');
+}
