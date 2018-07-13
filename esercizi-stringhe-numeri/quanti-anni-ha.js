6281679d006134369c3108bd35fafefe358ad133
Stefano <41064347+hiddenMonster@users.noreply.github.com>
2018-07-13 08:32:30 +0200

    risolto

--- esercizi-stringhe-numeri/quanti-anni-ha.js
+++ esercizi-stringhe-numeri/quanti-anni-ha.js
@@ -2,7 +2,6 @@
   Quanti anni ha?
   Scrivi un programma che prenda in input un anno di nascita e determini l'età della persona, calcola anche quanti anni sono necessari per raggiungere i 100. Stampa in output entrambi i risultati.
   http://www.imparareaprogrammare.it
-*/
 /****************
 "Quanti anni ha"*
 11/07/2018      *
