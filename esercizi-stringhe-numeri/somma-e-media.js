be633e59fa07d2f0e198db50882f1f661b46ce9d
Stefano <41064347+hiddenMonster@users.noreply.github.com>
2018-07-13 08:33:02 +0200

    risolto

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -2,7 +2,6 @@
   Somma e media
   Scrivi un programma che prenda in input 5 numeri a tua scelta e restituisca in output la somma e la media.
   http://www.imparareaprogrammare.it
-*/
 /****************
 "Somma e media" *
 11/07/2018      *
