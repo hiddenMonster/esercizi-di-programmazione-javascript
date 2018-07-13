85f55db4cd2ba2e525a8216e4d66098db1c7b293
Stefano <41064347+hiddenMonster@users.noreply.github.com>
2018-07-13 16:31:14 +0200

    risolto
    Una domanda scusami, ma in javascript come posso stampare senza andare a capo? In Java si usa System.out.print( "qualcosa");

--- esercizi-cicli/rincorri-la-decina.js
+++ esercizi-cicli/rincorri-la-decina.js
@@ -4,3 +4,29 @@
   Hint: per andare a capo stampa '\n'.
   http://www.imparareaprogrammare.it
 */
+/********************
+"Rincorri la decina"*
+13/07/2018          *
+By Ste              *
+*/
+
+//**********************************
+// con il ciclo for
+for(var i = 1; i < 31; i++)
+{
+  if (i % 10 == 1) {
+    console.log('\n');
+  }
+  console.log(i);
+}
+
+//**********************************
+// con il do while *****************
+var i = 1;
+do {
+  if (i % 10 == 1) {
+    console.log('\n');
+  }
+  console.log(i);
+  i++;
+} while (i < 31);
